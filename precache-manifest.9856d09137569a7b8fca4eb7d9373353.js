4cdb642dbb92df8e67efc28b15ec5c6aa6151d56
Om Krishna <omkrishna.ok@gmail.com>
2020-06-08 09:53:02 +0530

    Updates

---
+++ precache-manifest.9856d09137569a7b8fca4eb7d9373353.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383f6bf4ce50e4fb4e4b70afeb3c7344",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "486eac9b3114266d6667",
+    "url": "/monsters-rolodex/static/css/main.51e88e56.chunk.css"
+  },
+  {
+    "revision": "aff7973eb7dd44760977",
+    "url": "/monsters-rolodex/static/js/2.6f1b4d92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.6f1b4d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486eac9b3114266d6667",
+    "url": "/monsters-rolodex/static/js/main.64e6cdba.chunk.js"
+  },
+  {
+    "revision": "47289784df217b9fbbf8",
+    "url": "/monsters-rolodex/static/js/runtime-main.03711c5c.js"
+  }
+]);
